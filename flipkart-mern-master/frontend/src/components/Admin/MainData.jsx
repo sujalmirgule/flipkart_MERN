279398db26d0e8f2d717f69a1bdcbede91a80389
Sujal Mirgule <sujalmirgule00@gmail.com>
2026-09-10 21:18:58 +0530

    Fix frontend ESLint unused variable warnings for Vercel CI build

--- flipkart-mern-master/frontend/src/components/Admin/MainData.jsx
+++ flipkart-mern-master/frontend/src/components/Admin/MainData.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import Chart from 'chart.js/auto'
+import 'chart.js/auto';
 import { Doughnut, Line, Pie, Bar } from 'react-chartjs-2';
 import { getAdminProducts } from '../../actions/productAction';
 import { useSelector, useDispatch } from 'react-redux';
